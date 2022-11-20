04f0907c0daaef0b90c5ed3ce5db58d6f81801f9
Frida <f0573115753@gmail.com>
2022-11-21 01:09:58 +0200

    hw-question

--- js/hw_12.js
+++ js/hw_12.js
@@ -24,56 +24,23 @@ Firsts.push(Users[0]);
 Lasts.push(Users[0]);
 Idis.push(Users[0]);
 
+sortBy(Firsts, firstName);
+sortBy(Lasts, lastName);
+sortBy(Idis, Id);
 
-// sortBy(Firsts, firstName);
-// sortBy(Lasts, lastName);
-// sortBy(Idis, Id);
-
-// function sortBy(array) {
+function sortBy(array, ?) {
     for(let i = 1; i < Users.length; i++) {
-        let j =Firsts.length-1;
-        for(; j < Firsts.length && j >= 0; j--) {
+        let j =array.length-1;
+        for(; j < array.length && j >= 0; j--) {
             let obj = Users[i];
             let lastObj = Firsts[j];
-            if(obj.firstName > lastObj.firstName) {
-            Firsts.splice((j+1), 0, obj);
+            if(obj.? > lastObj.?) {
+            array.splice((j+1), 0, obj);
             break; 
             } else {
                 if((j) == 0) {
-                    Firsts.splice(0, 0, obj);
+                    array.splice(0, 0, obj);
                 }
-            }
-        }
-    }
-// }
-
-for(let i = 1; i < Users.length; i++) {
-    let j =Lasts.length-1;
-    for(; j < Lasts.length && j >= 0; j--) {
-        let obj = Users[i];
-        let lastObj = Lasts[j];
-        if(obj.lastName > lastObj.lastName) {
-        Lasts.splice((j+1), 0, obj);
-        break; 
-        } else {
-            if((j) == 0) {
-                Lasts.splice(0, 0, obj);
-            }
-        }
-    }
-}
-
-for(let i = 1; i < Users.length; i++) {
-    let j =Idis.length-1;
-    for(; j < Idis.length && j >= 0; j--) {
-        let obj = Users[i];
-        let lastObj = Idis[j];
-        if(obj.Id > lastObj.Id) {
-        Idis.splice((j+1), 0, obj);
-        break; 
-        } else {
-            if((j) == 0) {
-                Idis.splice(0, 0, obj);
             }
         }
     }
