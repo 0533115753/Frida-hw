e3fa4a7896fdb7f41c695abdf1e693e7d11ac187
0533115753 <118309688+0533115753@users.noreply.github.com>
2024-01-31 00:27:51 +0200

    Delete Lesson 13 directory

--- Lesson 13/js/hw_13.js
+++
@@ -1,6 +0,0 @@
-const div = document.querySelector("div");
-var color = prompt("Choose a color for the text (in English only)").toLowerCase().replaceAll(" ", "");
-ChangeColor();
-function ChangeColor() {
-    div.style.color = `${color}`;
-}   
